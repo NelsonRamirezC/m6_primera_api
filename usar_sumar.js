5fd3c771f50a60ab615c84cea2ce4f6e32d377e3
Nelson Ramirez <xhyskoax@gmail.com>
2023-06-12 21:32:14 -0400

    se incluye archivo usar_sumar.js y utils.js para demotrar importación y exportación

---
+++ usar_sumar.js
@@ -0,0 +1,5 @@
+const { sumar, restar } = require("./utils.js");
+
+console.log(sumar(2, 6));
+
+console.log(restar(2, 6));
---
+++ utils.js
@@ -0,0 +1,12 @@
+const sumar = (numero1, numero2) => {
+    return numero1 + numero2;
+};
+
+const restar = (numero1, numero2) => {
+    return numero1 - numero2;
+};
+
+module.exports = {
+    sumar,
+    restar,
+};
